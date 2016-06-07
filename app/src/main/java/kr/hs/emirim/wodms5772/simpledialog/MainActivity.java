3a26d356173b83592d3545176269cbdec9e886d8
Jaeeun Jung <wodms5772@naver.com>
2016-06-07 20:28:41 +0900

    #3 Create dialog.setPositive element

--- app/src/main/java/kr/hs/emirim/wodms5772/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/wodms5772/simpledialog/MainActivity.java
@@ -1,10 +1,12 @@
 package kr.hs.emirim.wodms5772.simpledialog;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -27,7 +29,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dialog.setTitle("First Dialog");
         dialog.setMessage("This is message part <3 ");
         dialog.setIcon(R.drawable.first_icon);
-        dialog.setPositiveButton("OK",null);
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"Button Click!!", Toast.LENGTH_LONG).show();//여기는 익명클래스니까 MainActivity붙이는거
+            }
+        });
         dialog.show();
     }
 }
